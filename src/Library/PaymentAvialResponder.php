47328a7e429e02e4f905dda08b2261e8f6239a2d
giunashvili <giorgi.official@yahoo.com>
2020-07-28 05:23:22 +0000

    refactor: instantantiating classes

--- src/Library/PaymentAvialResponder.php
+++ src/Library/PaymentAvialResponder.php
@@ -6,21 +6,50 @@ use Redberry\GeorgianCardGateway\Responses\PaymentAvail;
 
 class PaymentAvailResponder
 {
+  /**
+   * Request.
+   * 
+   * @var \Illuminate\Http\Request $request
+   */
   private $request;
+
+  /**
+   * Georgian card handler.
+   * 
+   * @var GeorgianCardHandler $handler
+   */
   private $handler;
 
+  /**
+   * build new instance.
+   * 
+   * @return self
+   */
+  public static function build()
+  {
+    return new self;
+  }
+
+  /**
+   * Construct georgian card handler.
+   */
   public function __construct()
   {
     $this -> handler = resolve( 'redberry.georgian-card.handler' );
     $this -> request = request();
   }
   
+  /**
+   * Build response object and respond.
+   * 
+   * @return XML
+   */
   public function respond()
   {
     $trxId         = $this -> request -> get( 'trx_id'       );
     $orderAmount   = $this -> request -> get( 'o_amount'     );
     $accountId     = $this -> request -> get( 'o_account_id' );
-    $chargerReport = $this -> request -> get( 'o_charger_report' );
+    $chargerReport = $this -> request -> get( 'o_charger_report' ) ?? 'No report';
 
     $paymentAvail = new PaymentAvail;
     $paymentAvail -> setResultCode( 1 );
@@ -29,7 +58,7 @@ class PaymentAvailResponder
     $paymentAvail -> setPurchaseShortDesc( 'order' );
     $paymentAvail -> setPurchaseLongDesc( $chargerReport );
     $paymentAvail -> setPurchaseAmount( $orderAmount );
-    $paymentAvail -> setAccountId( $accountId );
+    $accountId && $paymentAvail -> setAccountId( $accountId );
     
     $primaryTrxPcid = $this -> handler -> getPrimaryTransactionId( $this -> request );
 
